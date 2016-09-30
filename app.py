ab1707c7aff095db1f89397e6a5ba73f3e1941a3
Shamaul Dilmohamed <Shamaul Dilmohamed>
2016-09-30 10:51:33 -0400

    done

--- app.py
+++ app.py
@@ -3,8 +3,17 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def log():    
-    return render_template("login.html")
+def log():
+    return render_template("login.html")    
+
+@app.route("/auth", methods=["POST"])
+def auth():
+    user = "sham"
+    pas = "shammy"
+    res = "TRAITOR"
+    if user == request.form['user'] and pas == request.form['pass']:
+        res = "CONGRATS"
+    return render_template("results.html", result = res)
 
 
 if __name__ == "__main__":
